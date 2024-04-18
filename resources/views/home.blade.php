22bb9666d7aa3b9b0bc3224ec75441e7cab43f07
Mehedi5271 <mhassan201205@bscse.uiu.ac.bd>
2024-04-18 23:06:14 +0600

    2nd update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>User</title>
 </head>
 <body>
         <a href="{{route('users')}}">user</a>
