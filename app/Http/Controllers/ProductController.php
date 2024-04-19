f5ac8c0cc2a8f0a6fd4dd5e83b6a24c236320685
Mehedi5271 <mhassan201205@bscse.uiu.ac.bd>
2024-04-19 23:28:39 +0600

    product insert and show

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     function products(){
-        return view('admin.pages.products');
+       $products = Product::all();
+        return view('admin.pages.products',compact('products'));
     }
 
     function create(){
