6c6f85e45bbe2127fac53cd7f38e3a82363a8c7c
Mehedi5271 <mehedih5271@gmail.com>
2024-04-27 15:28:00 +0600

    Authetication complete

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,16 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+
+Route::middleware('auth')->prefix('admin')->group(function(){
 Route::get('/dashboard',[DashboardController::class,'dashboard'])->name('dashboard');
+
 Route::get('/products/trash',[ProductController::class,'trash'])->name('products.trash');
 Route::patch('/products/{id}/restore', [ProductController::class, 'restore'])->name('products.restore');
 Route::delete('/products/{id}/delete', [ProductController::class, 'delete'])->name('products.delete');
 Route::get('/products/downloadPdf',[ProductController::class,'downloadPdf'])->name('products.downloadPdf');
 Route::get('/products/downloadExcel',[ProductController::class,'downloadExcel'])->name('products.downloadExcel');
+
 
 Route::get('/products',[ProductController::class,'index'])->name('products.index');
 Route::get('/products/create',[ProductController::class,'create'])->name('products.create');
@@ -34,3 +38,5 @@ Route::get('/products/{id}',[ProductController::class,'show'])->name('products.s
 Route::get('/products/{id}/edit',[ProductController::class,'edit'])->name('products.edit');
 Route::patch('/products/{id}',[ProductController::class,'update'])->name('products.update');
 Route::delete('/products/{id}',[ProductController::class,'destroy'])->name('products.destroy');
+
+});
