05494ffc72e91a6c9d6cf776ab9c5dac1b79594e
Mehedi5271 <mehedih5271@gmail.com>
2024-06-01 17:05:36 +0600

    order start

---
+++ database/migrations/2024_06_01_094040_create_order_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id');
+            $table->string('product_title');
+            $table->decimal('unit_price');
+            $table->integer('quantity');
+            $table->unsignedBigInteger('color_id')->nullable();
+            $table->string('color_name')->nullable();
+            $table->timestamps();
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_products');
+
+    }
+};
