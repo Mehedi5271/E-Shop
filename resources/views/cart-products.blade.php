f900faf3e8731ebd91876d487a7d5e495f8392dd
Mehedi5271 <mehedih5271@gmail.com>
2024-06-06 16:13:25 +0600

    total price in cart

--- resources/views/cart-products.blade.php
+++ resources/views/cart-products.blade.php
@@ -17,15 +17,33 @@
                     <th   scope="row"> <span class="btn btn-sm btn-danger me-2 remove-btn"  >X</span> {{ $loop->iteration }}</th>
                     <td>{{ $cartProduct->product->title }} {{ $cartProduct->color ? ' - ' . $cartProduct->color->name : '' }}</td>
                     <td>{{ $cartProduct->quantity }}</td>
-                    <td>{{ $cartProduct->product->price }}</td>
+                    <td class="price">{{ $cartProduct->product->price }}</td>
                 </tr>
             @endforeach
+            <tr>
+                <td colspan="3"></td>
+                <td>Total: <span id="totalPrice">0</span></td>
+            </tr>
         </tbody>
     </table>
     @push('script')
     <script>
-        const removeBtn = document.querySelectorAll('.remove-btn')
-        console.log(removeBtn);
+       const removeBtn = document.querySelectorAll('.remove-btn');
+            removeBtn.forEach(function(btn) {
+             btn.addEventListener('click', function(){
+               btn.parentElement.parentElement.remove();
+                alert('Deleted')
+             })
+           });
+
+           const elementPrice = document.querySelectorAll('.price');
+               let totalPrice = 0;
+                elementPrice.forEach(function(element){
+                    totalPrice += parseFloat(element.innerText);
+                    document.getElementById('totalPrice').innerText = totalPrice;
+                })
+
+
     </script>
 
     @endpush
